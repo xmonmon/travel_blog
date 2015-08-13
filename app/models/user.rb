8adef132e8b33b6b652ec30f447cea20a2c66355
Monika Chumber <monikachumber@Monikas-MacBook-Pro.local>
2015-08-13 12:29:27 -0700

    avatar loads locally to profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ActiveRecord::Base
   has_many :posts
   has_secure_password
   validates :email, uniqueness: true
+  has_attached_file :avatar,
+  					:styles => { :medium => "150x150>", :thumb => "44x44>"},
+  					:default_url => "/images/:style/missing.png"
 
+  validates_attachment :avatar, :presence => true,
+  					   :content_type => { :content_type => ["image/jpeg", "image/gif", "image/png"]},
+  					   :size => { :in => 0..10.kilobytes }
 
   def full_name
     "#{self.first_name} #{self.last_name}"
